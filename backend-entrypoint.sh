7c58cc3bee9f2e1875cb21ad17ad25ed901014f4
Ankit kumar Gupta <ankitkumargupta@ip-192-168-29-183.ec2.internal>
2021-04-12 00:18:31 +0530

    remove unknown profile ul_stg

--- backend-entrypoint.sh
+++ backend-entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 FILEPATH=/etc/rudderstack/
-aws s3 cp --profile ul_stg ${WORKSPACECONFIG_S3_URI} ${FILEPATH}/workspaceConfig.json
+aws s3 cp ${WORKSPACECONFIG_S3_URI} ${FILEPATH}/workspaceConfig.json
 go run -mod=vendor main.go
